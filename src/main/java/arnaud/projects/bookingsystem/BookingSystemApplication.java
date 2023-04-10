437404f9d0a358508a75e9364336e69c7823a78e
Arnaud Mucyo <arnmuc98@gmail.com>
2023-04-10 19:27:25 +0200

    Initial commit

---
+++ src/main/java/arnaud/projects/bookingsystem/BookingSystemApplication.java
@@ -0,0 +1,13 @@
+package arnaud.projects.bookingsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookingSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookingSystemApplication.class, args);
+    }
+
+}
